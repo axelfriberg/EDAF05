ae183705e45406e4bffd111fb6fcd05ac2d3345b
Oskar holmberg <oskarholmberg@msn.com>
2014-04-03 15:02:54 +0200

    Started lab3

---
+++ lab3/SpanningUSA.java
@@ -0,0 +1,17 @@
+public class SpanningUSA{
+	public static void main(String[] args) {
+		Scanner scanner; 
+		HashMap<String, Integer> distance = new HashMap<>();
+		HashMap<String, HashMap<String, Integer>> cities = new HashMap<>();
+		
+		try{
+			scanner = new Scanner(new File(args[0]));
+			
+			while (scanner.hasNextLine()){
+				
+			}			
+		} catch (FileNotFoundException e){
+			e.printStackTrace();
+		}
+	}
+}
