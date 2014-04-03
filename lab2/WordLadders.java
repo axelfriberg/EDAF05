2eb26270b69edac70d1de0e8d4ec6a842c171856
Oskar holmberg <oskarholmberg@msn.com>
2014-04-03 13:48:43 +0200

    Finished lab2

--- lab2/WordLadders.java
+++ lab2/WordLadders.java
@@ -3,70 +3,74 @@ import java.io.*;
 
 public class WordLadders{
 	public static void main(String[] args) {
-		HashMap<String, LinkedList> words = new HashMap<>();
+		HashMap<String, LinkedList<String>> words = new HashMap<>();
 		ArrayList<Integer> nrStepsList = new ArrayList<>();
 		String word, bucketKey;
-		StringHandler sh = new StringHandler();
-
-		
-		Scanner s1 = new Scanner(System.in);
-
-		while (s1.hasNextLine()){
-			word = s1.nextLine();
-			for(int i = 0; i < word.length(); i++){
-				StringBuilder sb = new StringBuilder(word);
-				sb.deleteCharAt(i);
-				bucketKey = sh.sortString(sb.toString());
-				if(!words.containsKey(bucketKey)){
-					words.put(bucketKey, new LinkedList<>());
-				}
-				if(!words.get(bucketKey).contains(word)){
-					words.get(bucketKey).add(word);
+		Scanner s1, s2; 
+		try{
+			s1 = new Scanner(new File(args[0]));
+			s2 = new Scanner(new File(args[1]));
+			while (s1.hasNextLine()){
+				word = s1.nextLine();
+				for(int i = 0; i < word.length(); i++){
+					StringBuilder sb = new StringBuilder(word);
+					sb.deleteCharAt(i);
+					bucketKey = sortString(sb.toString());
+					if(!words.containsKey(bucketKey)){
+						words.put(bucketKey, new LinkedList<String>());
+					}
+					if(!words.get(bucketKey).contains(word)){
+						words.get(bucketKey).add(word);
+					}
 				}
 			}
+
+			while(s2.hasNextLine()){
+				String[] pair = s2.nextLine().split(" ");
+				int distance = getDistance(words, pair[0], pair[1]);
+				System.out.println(distance);
+			}
+		} catch (FileNotFoundException e){
+			e.printStackTrace();
 		}
 
-		Scanner s2 = new Scanner(System.in);
-
-		while(s2.hasNextLine()){
-			System.out.println(s2.nextLine());
-			String[] pair = s2.nextLine().split(" ");
-			System.out.println(pair.length);
-			int distance = sh.getDistance(words, pair[0], pair[1]);
-			System.out.println(distance);
-		}
 	}
 
-
-	public String sortString(String s){
+	private static String sortString(String s){
 		char[] chars = s.toCharArray();
        	Arrays.sort(chars);
        	String sorted = new String(chars);
   		return sorted;
 	}
 
-	public int getDistance(HashMap<String, LinkedList> words, String root, String find){
+	private static int getDistance(HashMap<String, LinkedList<String>> words, String root, String find){
 		LinkedList<String> queue = new LinkedList<>();
-		LinkedList<String> visited = new LinkedList<>(); 
+		HashSet<String> visited = new HashSet<>();
+		int currentNrNodes = 1;
+		int nextNrNodes = 0; 
 		int distance = 0;
 		queue.add(root);
 		while(!queue.isEmpty()){
 			String temp = queue.remove(0);
+			currentNrNodes--;
 			if(temp.equals(find)){
 				return distance;
 			}
-			distance++;
-			System.out.println(temp);
 			temp = sortString(temp.substring(1));
-			System.out.println(temp);
-			for(String s : words.get(temp)){
+			for(String s : words.get(temp)){	
 				if(!visited.contains(s)){
 					visited.add(s);
+					nextNrNodes++;
 					queue.add(s);
 				}
 			}
-
+			if(currentNrNodes == 0){
+				distance++;
+				currentNrNodes=nextNrNodes;
+				nextNrNodes=0;
+			}
 		}
-		return -1;
+		int notfound = -1;
+		return notfound;
 	}
 }
