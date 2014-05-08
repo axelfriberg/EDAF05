f43c7287558526a3bcc259a4a54a3fe70ef754f4
Oskar holmberg <oskarholmberg@msn.com>
2014-05-08 14:56:43 +0200

    Finished parsing lab5

--- lab5/DNA.java
+++ lab5/DNA.java
@@ -5,30 +5,48 @@ import java.lang.*;
 public class DNA{
 	public static void main(String[] args) {
 		HashMap<String, String> dnaMap = new HashMap<>();
+		HashMap<String, Integer> indexMap = new HashMap<>();
+		int[][] blosum = new int[24][24];
+		String[] protiens = {"A", "R", "N", "D" ,"C", "Q", "E", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T", "W", "Y", "V", "B", "Z", "X", "*"};
+		for(int i = 0; i<24; i++){
+			indexMap.put(protiens[i], i);
+		}
+
 
 		try{
 			Scanner scanner = new Scanner(new File(args[0]));
+			Scanner matrixScan = new Scanner(new File("../testfiles/lab5/BLOSUM62.txt"));
+			for (int i = 0; i < 7; i++) {
+				matrixScan.nextLine();
+			}
+			String row = "";
+			int k = 0;
+			while(matrixScan.hasNextLine()){
+				row = matrixScan.nextLine();
+				String[] matrixSplit = row.split("\\s+");
+				for (int i = 0; i < 24; i++) {
+					blosum [k][i] = Integer.parseInt(matrixSplit[i+1]);
+				}
+				k++;
+			}
+
+			String species = "";
 			while(scanner.hasNextLine()){
 				String line = scanner.nextLine();
-				String species = "";
+				StringBuilder dna = new StringBuilder();
 				if(line.contains(">")){
 					String[] split = line.split("\\s+"); 
 					species = split[0].substring(1);
+					dnaMap.put(species, "");
 				}
-				
-				StringBuilder dna = new StringBuilder();
-
-				while(!line.contains(">")){
+				else{
+					dna.append(dnaMap.get(species));
 					dna.append(line);
-					line = scanner.nextLine();
+					dnaMap.put(species, dna.toString());
 				}
-				dnaMap.put(species, dna.toString());
-			}
-
-			for (String s : dnaMap.keySet()) {
-				System.out.println(s + " " + dnaMap.get(s));
 			}
 		}
+
 		catch (FileNotFoundException e){
 		e.printStackTrace();
 		}
